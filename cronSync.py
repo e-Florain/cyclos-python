91d27be6af2fadd83042d0a0fe8157d2bb5d62fd
Guillaume Roche <groche@guigeek.org>
2022-09-12 14:08:05 +0200

    Changes for script cron

--- cronSync.py
+++ cronSync.py
@@ -8,41 +8,57 @@ import random
 import string
 import json
 import os
+import config as cfg
 
 o2c = Odoo2Cyclos()
 keysjsons = dict()
 letters = string.ascii_lowercase
+url = cfg.hostname['url']
+smtp = cfg.smtp['ip']
 
 msg = EmailMessage()
 o2c.simulate = True
+# Get Changes Adhs
 filename = o2c.getChangesAdhs()
 key = ''.join(random.choice(letters) for i in range(64))
 keysjsons[key] = filename
 str = ""
-with open(os.path.dirname(os.path.abspath(__file__))+"/json/"+filename) as fp:
-    str = str+fp.read()
-str = str + "\n"
-str = str + "Pour valider ces changements, veuillez cliquer sur le lien suivant : http://10.0.3.184:8080/allow/"+key
-str = str + "\n"
+with open(os.path.dirname(os.path.abspath(__file__))+"/json/"+filename) as data_file:
+    arr = json.load(data_file)
+if bool(arr):
+    with open(os.path.dirname(os.path.abspath(__file__))+"/json/"+filename) as fp:
+        str = str+fp.read()
+    str = str + "\n"
+    str = str + "Pour valider ces changements, veuillez cliquer sur le lien suivant : http://"+url+"/allow/"+key
+    str = str + "\n"
+
+str = str + "---------------------------------------------------------------------------------------\n"
+str = str + "---------------------------------------------------------------------------------------\n"
+
+# Get Changes Adhs Pros
 filename = o2c.getChangesAdhPros()
 key = ''.join(random.choice(letters) for i in range(64))
 keysjsons[key] = filename
-with open(os.path.dirname(os.path.abspath(__file__))+"/json/"+filename) as fp:
-    str = str+fp.read()
-str = str + "\n"
-str = str + "Pour valider ces changements, veuillez cliquer sur le lien suivant : http://10.0.3.184:8080/allow/"+key
+with open(os.path.dirname(os.path.abspath(__file__))+"/json/"+filename) as data_file:
+    arr = json.load(data_file)
+if bool(arr):
+    with open(os.path.dirname(os.path.abspath(__file__))+"/json/"+filename) as fp:
+        str = str+fp.read()
+    str = str + "\n"
+    str = str + "Pour valider ces changements, veuillez cliquer sur le lien suivant : http://"+url+"/allow/"+key
 
-with open(os.path.dirname(os.path.abspath(__file__))+'/url.key', 'w') as outfile:
-    json.dump(keysjsons, outfile, indent=4, sort_keys=False, separators=(',', ':'))
-print(str)
+    with open(os.path.dirname(os.path.abspath(__file__))+'/url.key', 'w') as outfile:
+        json.dump(keysjsons, outfile, indent=4, sort_keys=False, separators=(',', ':'))
+#print(str)
 msg.set_content(str)
 # me == the sender's email address
 # you == the recipient's email address
 msg['Subject'] = f'Attention modifications à faire sur cyclos'
+#msg['From'] = "odoo@eflorain.fr"
 msg['From'] = "odoo@eflorain.fr"
 msg['To'] = "groche@guigeek.org"
 
 # Send the message via our own SMTP server.
-s = smtplib.SMTP('192.168.100.15')
-#s.send_message(msg)
+s = smtplib.SMTP(smtp)
+s.send_message(msg)
 s.quit()
