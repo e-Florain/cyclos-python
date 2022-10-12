b9dcff71212a24a8c4b826ba27ba90dc5bca6a26
Guillaume Roche <groche@guigeek.org>
2022-10-12 20:59:15 +0200

    Add lock to access to url.key

--- cronSync.py
+++ cronSync.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from filelock import FileLock
 from Odoo2Cyclos import Odoo2Cyclos
 import smtplib
 from email.message import EmailMessage
@@ -47,16 +48,14 @@ if bool(arr):
     str = str + "\n"
     str = str + "Pour valider ces changements, veuillez cliquer sur le lien suivant : "+url+"/allow/"+key
 
-with open(os.path.dirname(os.path.abspath(__file__))+'/url.key', 'w') as outfile:
-    json.dump(keysjsons, outfile, indent=4, sort_keys=False, separators=(',', ':'))
+with FileLock(os.path.dirname(os.path.abspath(__file__)) + "/myfile2.txt"):
+    with open(os.path.dirname(os.path.abspath(__file__))+'/url.key', 'w') as outfile:
+        json.dump(keysjsons, outfile, indent=4, sort_keys=False, separators=(',', ':'))
 #print(str)
 msg.set_content(str)
-# me == the sender's email address
-# you == the recipient's email address
 msg['Subject'] = f'Attention modifications à faire sur cyclos'
-#msg['From'] = "odoo@eflorain.fr"
 msg['From'] = "odoo@eflorain.fr"
-msg['To'] = "groche@guigeek.org"
+msg['To'] = "tech@florain.fr"
 
 if (str != ""):
     s = smtplib.SMTP(smtp)
